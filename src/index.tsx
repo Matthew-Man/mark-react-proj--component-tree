99ad9580e76f1883916bca553380739384a010c7
Richard Ng <richardcrng@gmail.com>
2020-12-31 11:09:01 +0000

    Remove separate App.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const developer = {
+  firstName: "Arbelia",
+  lastName: "Arkansas",
+  company: "Martco PLC",
+};
+const dayNumber = 4;
+const tickets = ["Log on", "Do something else", "Debug this code"];
+
+function App() {
+  return (
+    <div>
+      <h1>Development log: day {dayNumber}</h1>
+      <ul>
+        <li>
+          <b>Developer name: </b>
+          {developer.lastName.toUpperCase() + ", " + developer.firstName}
+        </li>
+        <li>
+          <b>Company: </b>
+          {developer.company}
+        </li>
+      </ul>
+      <hr />
+      <h2>Ticket status</h2>
+      <p>
+        There are currently <b>{tickets.length}</b> tickets left to complete.
+      </p>
+      <p>
+        <b>Your next ticket:</b> {tickets[0]}
+      </p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
